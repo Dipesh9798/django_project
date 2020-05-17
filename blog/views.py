c2db34145083a197b30324db97100a64fded836c
Dipesh <dipeshad9798@gmail.com>
2020-05-17 12:25:13 +0530

    Added templates

---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts=[
+    {
+        'author':'Dipesh',
+        'title':'MyFirstBook',
+        'content':'First post content',
+        'date_posted':'17 May 2020'
+
+    },
+    {
+        'author':'Paul Coelho',
+        'title':'The Alchemist',
+        'content':'Second post content',
+        'date_posted':'17 May 2005'
+
+    }
+
+]
+
+def home(request):
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
+
