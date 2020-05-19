c1b9e35072e4f3d99976ef3a987ab7f1eb982c71
Dipesh <dipeshad9798@gmail.com>
2020-05-19 15:09:54 +0530

    created db model blog-Post

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts=[
-    {
-        'author':'Dipesh',
-        'title':'MyFirstBook',
-        'content':'First post content',
-        'date_posted':'17 May 2020'
-
-    },
-    {
-        'author':'Paul Coelho',
-        'title':'The Alchemist',
-        'content':'Second post content',
-        'date_posted':'17 May 2005'
-
-    }
-
-]
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
